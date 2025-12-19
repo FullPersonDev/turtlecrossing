df19d87e710767c5921b7d44ed3eb8cd11e6289e
Martin Aguero <martin.aguero84@gmail.com>
2025-12-19 05:11:15 -0600

    added logic for player and car manager classes

--- car.py
+++ car.py
@@ -1,6 +1,26 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "yellow", "blue", "orange", "green", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.all_cars = []
+        self.hideturtle()
+
+    def create_car(self):
+        new_car = Turtle("square")
+        new_car.color(random.choice(COLORS))
+        new_car.penup()
+        new_car.resizemode("user")
+        new_car.shapesize(stretch_wid=1, stretch_len=2)
+        new_car.setheading(180)
+        ran_y = random.randint(-250, 250)
+        new_car.goto(280, ran_y)
+        self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.forward(STARTING_MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from turtle import Screen
 import time
+from turtle import Screen
 from player import Player
 from car import CarManager
 from scoreboard import Scoreboard
@@ -10,9 +10,27 @@ screen.setup(600, 600)
 screen.title("Welcome to Turtle Crossing")
 screen.tracer(0)
 
+# Create player, car manager, and scoreboard
+player = Player()
+car_manager = CarManager()
+
+# Listen for key presses
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
 game_is_on = True
+turn = 0
 while game_is_on:
-    time.sleep(0.1)
+    # Refresh screen animation
     screen.update()
+    time.sleep(0.1)
+
+    if turn % 6 == 0:
+        car_manager.create_car()
+
+    car_manager.move_cars()
+    player.refresh_turtle()
+
+    turn += 1
 
 screen.exitonclick()
